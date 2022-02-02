b12e95920e3f7e5cdc7d15a70c100bd0ef5320a0
Tamirat worku Lemma <tlemma@3-B-1.42heilbronn.de>
2022-02-02 18:33:00 +0100

    data structures added

---
+++ src/Todo.md
@@ -0,0 +1,16 @@
+## Todo
+	- parse_quotes 
+    	- check quotes are closed and return an error for unclosed quotes
+  	- Check if cmd is unquoted in lexer
+    	- if not move to the nect cmd word
+  	- Build Exec Env
+    	- Open files inherited upon invocation of the shell, plus open files controlled by exec
+    	- Working directory as set by cd
+    	- File creation mask set by umask
+    	- [XSI] [Option Start] File size limit as set by ulimit [Option End]
+    	- Current traps set by trap
+    	- environment inherited by the shell when it begins (see the export special built-in)
+    	- Process IDs of the last commands in asynchronous lists known to this shell environment; see Asynchronous Lists
+
+
+## Done
--- src/lexer.c
+++ src/lexer.c
@@ -6,7 +6,7 @@
 /*   By: tlemma <tlemma@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 16:26:02 by tlemma            #+#    #+#             */
-/*   Updated: 2022/02/01 18:52:23 by tlemma           ###   ########.fr       */
+/*   Updated: 2022/02/02 16:50:14 by tlemma           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,4 +26,33 @@ typedef struct s_cmd_table
 	char	*infile;
 	char	*outfile;
 } 		t_cmd_table;
+
+enum e_IFS{
+	WSPACE=' ',
+	TAB='\t',
+	NEWLINE='\n',
+};
+
+enum e_TOKENS{
+	WORD=-1,
+	FLAG='-',
+	QUOTE='\'',
+	DQUOTE='\"',
+	LESS='<',
+	GREAT='>',
+	PIPE='|',
+	INFILE=-2,
+	OUTFILE=-3,
+	DOLLAR='$',
+	QMARK='?',
+	BOPEN='(',
+	BCLOSE=')',
+};
+
+enum	e_STATE{
+	INQUOTE,
+	OUTQUOTE,
+	INDQUOTE,
+	OUTDQUOTE,
+};
 
